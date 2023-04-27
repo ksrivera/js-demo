0af564f1e08ddb9084d38f8c3d9796155d5d659c
ksrivera <ksrivera7@gmail.com>
2023-04-27 15:35:27 +0800

    comments

---
+++ 01_Variable_Datatypes/01Console/script.js
@@ -0,0 +1,30 @@
+console.log(1);
+
+// Log number
+console.log(100);
+
+// Log string
+console.log('Hello World');
+
+//Log multiple values
+console.log(20, 'Hello', true);
+
+//Log a variable
+const x = 100;
+console.log(x);
+
+
+//console error & warning
+console.error('Alert');
+console.warn("Warning");
+
+//log object as a table
+console.table({name: "Kenneth", email: "ksrivera7@gmail.com"});
+
+
+//Group console commands
+console.group('simple');
+console.warn('warning');
+console.alert('alert');
+console.log(x);
+console.groupEnd();
