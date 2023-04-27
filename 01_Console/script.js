9eb6aa8161c50a08eb98660461dfafde9b67e3ad
ksrivera <ksrivera7@gmail.com>
2023-04-27 15:12:36 +0800

    js console

---
+++ 01_Console/script.js
@@ -0,0 +1,18 @@
+console.log(1);
+
+// Log number
+console.log(100);
+
+// Log string
+console.log('Hello World');
+
+
+console.log(20, 'Hello', true);
+
+console.warn('warn');
+console.alert();
+
+console.group('simple');
+console.warn('warning');
+console.alert('alert');
+console.groupEnd();
