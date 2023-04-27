feb69e09f428568e247ddcbe0cd20fe6c1c2a82e
ksrivera <ksrivera7@gmail.com>
2023-04-27 16:27:55 +0800

    updated

---
+++ 01_Variable_Datatypes/03/script.js
@@ -0,0 +1,9 @@
+//Single line of code comment
+
+//The shortcut key is: ctrl + /
+
+//Multiline comment shortcut key: Shift + Alt + A
+
+/* console.log(100);
+console.log("Hellow World");
+console.log(20, "Hello", true);  */
